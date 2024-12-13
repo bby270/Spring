94273b2bb06374f1a713c870c45b2ebc8543ae59
aldnjs <sunbest2003@gmail.com>
2024-12-13 12:35:03 +0900

    spring

---
+++ jdbcapp/src/main/java/com/dw/jdbcapp/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.dw.jdbcapp.controller;
+
+import com.dw.jdbcapp.model.Employee;
+import com.dw.jdbcapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/find-all-employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+    // Quety param
+    @GetMapping("/employee")
+    public Employee getEmployeeById(@RequestParam String id) {
+        return employeeService.getEmployeeById(id);
+    }
+}
