95c57611c5e95e13ce0c07a7e9a484bf5de00f99
aldnjs <sunbest2003@gmail.com>
2024-12-26 14:48:52 +0900

    Spring

---
+++ jpaapp/src/main/java/com/dw/jpaapp/model/Course.java
@@ -0,0 +1,24 @@
+package com.dw.jpaapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title",nullable = false)
+    private String title;
+    @Column(name = "description")
+    private String description;
+}
---
+++ jpaapp/src/main/java/com/dw/jpaapp/model/Instructor.java
@@ -0,0 +1,24 @@
+package com.dw.jpaapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name",nullable = false)
+    private String name;
+    @Column(name = "career")
+    private String career;
+}
