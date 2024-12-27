e45ec1b4c195b1aaab09cf41ca12b77de357c36b
aldnjs <sunbest2003@gmail.com>
2024-12-27 09:47:41 +0900

    Spring

---
+++ jpaapp/src/main/java/com/dw/jpaapp/repository/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.dw.jpaapp.repository;
+
+import com.dw.jpaapp.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course,Long> {
+
+}
