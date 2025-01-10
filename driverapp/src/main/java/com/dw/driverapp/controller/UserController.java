f91f519419c0f5b55e9277b7f1f282793541d958
aldnjs <sunbest2003@gmail.com>
2025-01-10 11:08:14 +0900

    Spring

--- driverapp/src/main/java/com/dw/driverapp/controller/UserController.java
+++ driverapp/src/main/java/com/dw/driverapp/controller/UserController.java
@@ -1,24 +1,29 @@
 package com.dw.driverapp.controller;
 
+import com.dw.driverapp.dto.UserDTO;
 import com.dw.driverapp.model.User;
-import com.dw.driverapp.service.UserService;
+import com.dw.driverapp.service.UserSevice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 public class UserController {
     @Autowired
-   private UserService userService;
+    UserSevice userSevice;
 
-    @PostMapping("/register")
-    public ResponseEntity<User>  getRegister (@RequestBody User user) {
-        return new ResponseEntity<>(userService.getRegister(user),HttpStatus.OK);
+    @PostMapping("/users/register")
+    public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO){
+        return new ResponseEntity<>(userSevice.registerUser(userDTO), HttpStatus.CREATED);
+    }
 
+    @GetMapping("/user/me")
+    public ResponseEntity<List<User>>getAllUser() {
+        return new ResponseEntity<>(userSevice.getAllUser(),HttpStatus.OK);
     }
 }
+
--- driverapp/src/main/java/com/dw/driverapp/repository/UserRepository.java
+++ driverapp/src/main/java/com/dw/driverapp/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.dw.driverapp.repository;
 import com.dw.driverapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
