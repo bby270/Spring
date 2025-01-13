8c0aa98bd0d314e7ae8d716c55268433072335c9
aldnjs <sunbest2003@gmail.com>
2025-01-13 10:42:33 +0900

    Spring

--- driverapp/src/main/resources/data.sql
+++ driverapp/src/main/resources/data.sql
@@ -1,3 +1,16 @@
-INSERT INTO authority (authority_name) VALUES ('ADMIN');
-INSERT INTO authority (authority_name) VALUES ('USER');
+INSERT INTO Image (image_url)
 
+
+
+INSERT INTO 자동차종류 (id,name)
+VALUES
+('승용차','1톤화물차','대형버스','오토바이 125CC 이상고배기량')
+
+INSERT INTO 이미지 (image_url)
+VALUES
+('car photo_url','motor cycle photo_url','bus photo_url')
+
+INSERT INTO 비디오 (video_url)
+VALUES
+('Study video_url(1종보통)','Study video_url(2종보통)','Study video_url(1종대형)',
+'Study video_url(2종소형)')
